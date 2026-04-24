f1ee3d6241313c327635991254fd777315bbf432
570237037-beep <570237037@qq.com>
2026-04-24 19:59:21 +0800

    Create doubao.js

---
+++ api/doubao.js
@@ -0,0 +1,31 @@
+export default {
+  async fetch(request) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
+      });
+    }
+
+    const body = await request.json();
+    const response = await fetch('https://ark.cn-beijing.volces.com/api/v3/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ark-7369c807-376a-443f-b43f-3bd16c34c4dd-64e0c',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
+};
